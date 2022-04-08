6091416fc9624b787cbdfb77df7d12a2fd245216
sunnytoolsmiths <xx252@cornell.edu>
2022-04-07 21:06:17 -0700

    add basic health points functions for the player and enemy

---
+++ assets/js/game.js
@@ -0,0 +1,39 @@
+// this creates a function named fight
+
+var playerName = window.prompt ("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto" ;
+var enemyHealth = 50;
+var enemyAttack = 12; 
+
+var fight = function(){
+    window.alert("Welcome to Robot Gladiators!");
+    
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + "attacked" + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining.");
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack; 
+    console.log (enemyName + "attacked" + playerName + ". "+ playerName + " now has " + playerHealth + " health remaining.");
+    if (playerHealth <= 0){
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert (playerName + "still has " + playerHealth + " health left. ");
+    }
+}    
+
+fight ()
+
+
+
+
